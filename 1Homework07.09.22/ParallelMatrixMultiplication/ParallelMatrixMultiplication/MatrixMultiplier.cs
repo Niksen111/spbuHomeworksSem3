0f085e044ab707395584cd738c3d4aeddc932c88
Niksen111 <nikiksen2003@gmail.com>
2022-09-20 16:17:44 +0300

    writing of the work of threads has begun

--- 1Homework07.09.22/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixMultiplier.cs
+++ 1Homework07.09.22/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixMultiplier.cs
@@ -11,29 +11,54 @@ public static class MatrixMultiplier
     private static List<List<int>> _outputMatrix;
     private static List<List<int>> _matrix1;
     private static List<List<int>> _matrix2;
+    private static List<((int row,  int column) start, (int row, int column) end)> _threadsActions;
+    private static Mutex _mutex;
+    private static (int rows, int columns) _matrix1Size;
+    private static (int rows, int columns) _matrix2Size;
+    private static (int rows, int columns) _outputMatrixSize;
 
+    
     static MatrixMultiplier()
     {
         _outputMatrix = new List<List<int>>();
         _matrix1 = new List<List<int>>();
         _matrix2 = new List<List<int>>();
+        _threadsActions = new List<((int row, int column) start, (int row, int column) end)>();
+        _mutex = new Mutex();
 
-        for (int i = 0; i < ThreadsCount; ++i)
+        for (int index = 0; index < ThreadsCount; ++index)
         {
-            
+            var i = index;
+            _threads[i] = new Thread(() =>
+            {
+                while (true)
+                {
+                    while (_threadsActions[i].end.row < 0)
+                    {
+                        _mutex.WaitOne();
+                    }
+
+                    for (int j = _threadsActions[i].start.row; j <= _threadsActions[i].end.row; ++j)
+                    {
+                        for (int k = _threadsActions[i].start.column;
+                             k < _outputMatrixSize.columns &&
+                             (j < _threadsActions[i].end.row || k <= _threadsActions[i].end.column); ++k)
+                        {
+                            
+                        }
+                    }
+
+                    _threadsActions[i] = ((-1, -1), (-1, -1));
+                }
+            });
         }
     }
 
-    private static void Multiply(int rowStart, int columnStart, int numberElements)
-    {
-        
-    }
-    
     private static List<List<int>> GetMatrixFromFile(string path)
     {
         var lines = File.ReadAllLines(path);
-
         var matrix = new List<List<int>>();
+        
         for (int i = 0; i < lines.Length; ++i)
         {
             matrix.Add(new List<int>());
@@ -60,7 +85,19 @@ public static class MatrixMultiplier
         {
             throw (FailedReadMatricesException) e;
         }
+
+        _matrix1Size = (_matrix1.Count, _matrix1[0].Count);
+        _matrix2Size = (_matrix2.Count, _matrix2[0].Count);
+        _outputMatrixSize = (_matrix1Size.rows, _matrix2Size.columns);
+        _outputMatrix = new List<List<int>>();
         
-        
+        for (int i = 0; i < _outputMatrixSize.rows; ++i)
+        {
+            _outputMatrix.Add(new List<int>());
+            for (int j = 0; j < _outputMatrixSize.columns; ++j)
+            {
+                
+            }
+        }
     }
 }
