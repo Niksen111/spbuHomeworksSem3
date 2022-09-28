810b3816bbcb4b20c4ee4fd23897892294ad33fd
Niksen111 <nikiksen2003@gmail.com>
2022-09-28 15:13:51 +0300

    Added tests project

---
+++ 2Homework14.09.22/Lazy/Lazy.Tests/LazyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Lazy.Tests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
