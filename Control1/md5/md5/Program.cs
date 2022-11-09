0075e4e71c52dfa1159c7bf6a9dab8e92759d58d
Niksen111 <nikiksen2003@gmail.com>
2022-11-09 12:56:13 +0300

    Added timer

--- Control1/md5/md5/Program.cs
+++ Control1/md5/md5/Program.cs
@@ -1,12 +1,26 @@
-﻿namespace md5;
+﻿using System.Diagnostics;
+using static System.Diagnostics.Stopwatch;
+
+
+namespace md5;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var x1 = CheckSum.GetCheckSum("../../");
-        var x2 = CheckSum.GetCheckSumParallel("../../").Result;
+        long time1;
+        long time2;
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var x1 = CheckSum.GetCheckSum("../../../../../../");
+        stopwatch.Stop();
+        time1 = stopwatch.ElapsedMilliseconds;
+        stopwatch.Reset();
+        stopwatch.Start();
+        var x2 = CheckSum.GetCheckSumParallel("../../../../../../").Result;
+        stopwatch.Stop();
+        time2 = stopwatch.ElapsedMilliseconds;
 
-        Console.WriteLine();
+        Console.WriteLine($"One-thread {time1}, parallel {time2}");
     }
 }
