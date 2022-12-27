62e0fb2eca05491ce1f1ac4f1b087e9951d1e0e7
Niksen111 <nikiksen2003@gmail.com>
2022-12-27 13:28:58 +0300

    Little changes

--- 1Homework07.09.22/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixMultiplier.cs
+++ 1Homework07.09.22/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixMultiplier.cs
@@ -6,22 +6,22 @@
 /// </summary>
 public static class MatrixMultiplier
 {
-    public static int ThreadsCount => Environment.ProcessorCount;
-    private static Thread[] _threads;
-    private static List<List<int>> _outputMatrix;
-    private static List<List<int>> _matrix1;
-    private static List<List<int>> _matrix2;
-    private static List<((int row,  int column) start, (int row, int column) end)> _threadsActions;
-    private static (int rows, int columns) _matrix1Size;
-    private static (int rows, int columns) _matrix2Size;
-    private static (int rows, int columns) _outputMatrixSize;
+    public static int ThreadsCount => 15;
+    private static Thread[] threads;
+    private static List<List<int>> outputMatrix;
+    private static List<List<int>> matrix1;
+    private static List<List<int>> matrix2;
+    private static readonly List<((int row,  int column) start, (int row, int column) end)> _threadsActions;
+    private static (int rows, int columns) matrix1Size;
+    private static (int rows, int columns) matrix2Size;
+    private static (int rows, int columns) outputMatrixSize;
 
     static MatrixMultiplier()
     {
-        _threads = new Thread[ThreadsCount];
-        _outputMatrix = new List<List<int>>();
-        _matrix1 = new List<List<int>>();
-        _matrix2 = new List<List<int>>();
+        threads = new Thread[ThreadsCount];
+        outputMatrix = new List<List<int>>();
+        matrix1 = new List<List<int>>();
+        matrix2 = new List<List<int>>();
         _threadsActions = new List<((int row, int column) start, (int row, int column) end)>();
         
         RefreshThreads();
@@ -37,7 +37,7 @@ public static class MatrixMultiplier
                 _threadsActions.Add(((-1, -1), (-1, -1)));
             }
 
-            _threads[i] = new Thread(() =>
+            threads[i] = new Thread(() =>
             {
                 if (_threadsActions[i].end.row == -1)
                 {
@@ -47,14 +47,14 @@ public static class MatrixMultiplier
                 for (int j = _threadsActions[i].start.row; j <= _threadsActions[i].end.row; ++j)
                 {
                     for (int k = j == _threadsActions[i].start.row ? _threadsActions[i].start.column : 0;
-                         k < _outputMatrixSize.columns && j < _threadsActions[i].end.row 
+                         k < outputMatrixSize.columns && j < _threadsActions[i].end.row 
                          || k <= _threadsActions[i].end.column && j == _threadsActions[i].end.row;
                          ++k)
                     {
-                        _outputMatrix[j][k] = 0;
-                        for (int l = 0; l < _matrix1Size.columns; ++l)
+                        outputMatrix[j][k] = 0;
+                        for (int l = 0; l < matrix1Size.columns; ++l)
                         {
-                            _outputMatrix[j][k] += _matrix1[j][l] * _matrix2[l][k];
+                            outputMatrix[j][k] += matrix1[j][l] * matrix2[l][k];
                         }
                     }
                 }
@@ -80,8 +80,8 @@ public static class MatrixMultiplier
 
     private static void CheckMatrices(List<List<int>> matrix1, List<List<int>> matrix2)
     {
-        (int rows, int columns) matrix1Size = (matrix1.Count, _matrix1[0].Count);
-        (int rows, int columns) matrix2Size = (matrix2.Count, _matrix2[0].Count);
+        (int rows, int columns) matrix1Size = (matrix1.Count, MatrixMultiplier.matrix1[0].Count);
+        (int rows, int columns) matrix2Size = (matrix2.Count, MatrixMultiplier.matrix2[0].Count);
 
         if (matrix1Size.columns != matrix2Size.rows)
         {
@@ -115,26 +115,26 @@ public static class MatrixMultiplier
     {
         RefreshThreads();
 
-        _matrix1 = GetMatrixFromFile(matrix1Path);
-        _matrix2 = GetMatrixFromFile(matrix2Path);
+        matrix1 = GetMatrixFromFile(matrix1Path);
+        matrix2 = GetMatrixFromFile(matrix2Path);
         
-        CheckMatrices(_matrix1, _matrix2);
+        CheckMatrices(matrix1, matrix2);
         
-        _matrix1Size = (_matrix1.Count, _matrix1[0].Count);
-        _matrix2Size = (_matrix2.Count, _matrix2[0].Count);
-        _outputMatrixSize = (_matrix1Size.rows, _matrix2Size.columns);
-        _outputMatrix = new List<List<int>>();
+        matrix1Size = (matrix1.Count, matrix1[0].Count);
+        matrix2Size = (matrix2.Count, matrix2[0].Count);
+        outputMatrixSize = (matrix1Size.rows, matrix2Size.columns);
+        outputMatrix = new List<List<int>>();
         
-        for (int i = 0; i < _outputMatrixSize.rows; ++i)
+        for (int i = 0; i < outputMatrixSize.rows; ++i)
         {
-            _outputMatrix.Add(new List<int>());
-            for (int j = 0; j < _outputMatrixSize.columns; ++j)
+            outputMatrix.Add(new List<int>());
+            for (int j = 0; j < outputMatrixSize.columns; ++j)
             {
-                _outputMatrix[i].Add(0);
+                outputMatrix[i].Add(0);
             }
         }
 
-        int size = _outputMatrixSize.columns * _outputMatrixSize.rows;
+        int size = outputMatrixSize.columns * outputMatrixSize.rows;
         int distributedNow = -1;
         int step = (size + ThreadsCount - 1) / ThreadsCount;
         int currentThread = 0;
@@ -143,27 +143,27 @@ public static class MatrixMultiplier
         {
             int currentStep = Math.Min(step, size - distributedNow);
             
-            _threadsActions[currentThread] = (((distributedNow + 1) / _outputMatrixSize.columns, (distributedNow + 1) % _outputMatrixSize.columns), 
-                ((distributedNow + currentStep) / _outputMatrixSize.columns, (distributedNow + currentStep) % _outputMatrixSize.columns)); 
+            _threadsActions[currentThread] = (((distributedNow + 1) / outputMatrixSize.columns, (distributedNow + 1) % outputMatrixSize.columns), 
+                ((distributedNow + currentStep) / outputMatrixSize.columns, (distributedNow + currentStep) % outputMatrixSize.columns)); 
             
             distributedNow += currentStep;
             ++currentThread;
         }
 
         --currentThread;
-        _threadsActions[currentThread] = ((_threadsActions[currentThread].start), (_threadsActions[currentThread].end.row - 1, _outputMatrixSize.columns - 1));
+        _threadsActions[currentThread] = ((_threadsActions[currentThread].start), (_threadsActions[currentThread].end.row - 1, outputMatrixSize.columns - 1));
 
         for (int i = 0; i < ThreadsCount; ++i)
         {
-            _threads[i].Start();
+            threads[i].Start();
         }
 
         for (int i = 0; i < ThreadsCount; ++i)
         {
-            _threads[i].Join();
+            threads[i].Join();
         }
 
-        var output = _outputMatrix.Select(n => string.Join(" ", n.Select(m => m.ToString()).ToArray()));
+        var output = outputMatrix.Select(n => string.Join(" ", n.Select(m => m.ToString()).ToArray()));
         
         File.WriteAllLines(outputPath, output);
     }
@@ -176,38 +176,38 @@ public static class MatrixMultiplier
     /// <param name="outputPath">path to the output file</param>
     public static void MultiplyOneThreaded(string matrix1Path, string matrix2Path, string outputPath)
     {
-        _matrix1 = GetMatrixFromFile(matrix1Path);
-        _matrix2 = GetMatrixFromFile(matrix2Path);
+        matrix1 = GetMatrixFromFile(matrix1Path);
+        matrix2 = GetMatrixFromFile(matrix2Path);
         
-        CheckMatrices(_matrix1, _matrix2);
+        CheckMatrices(matrix1, matrix2);
         
-        _matrix1Size = (_matrix1.Count, _matrix1[0].Count);
-        _matrix2Size = (_matrix2.Count, _matrix2[0].Count);
-        _outputMatrixSize = (_matrix1Size.rows, _matrix2Size.columns);
-        _outputMatrix = new List<List<int>>();
+        matrix1Size = (matrix1.Count, matrix1[0].Count);
+        matrix2Size = (matrix2.Count, matrix2[0].Count);
+        outputMatrixSize = (matrix1Size.rows, matrix2Size.columns);
+        outputMatrix = new List<List<int>>();
         
-        for (int i = 0; i < _outputMatrixSize.rows; ++i)
+        for (int i = 0; i < outputMatrixSize.rows; ++i)
         {
-            _outputMatrix.Add(new List<int>());
-            for (int j = 0; j < _outputMatrixSize.columns; ++j)
+            outputMatrix.Add(new List<int>());
+            for (int j = 0; j < outputMatrixSize.columns; ++j)
             {
-                _outputMatrix[i].Add(0);
+                outputMatrix[i].Add(0);
             }
         }
         
-        for (int i = 0; i < _outputMatrixSize.rows; ++i)
+        for (int i = 0; i < outputMatrixSize.rows; ++i)
         {
-            for (int j = 0; j < _outputMatrixSize.columns; ++j)
+            for (int j = 0; j < outputMatrixSize.columns; ++j)
             {
-                _outputMatrix[i][j] = 0;
-                for (int l = 0; l < _matrix1Size.columns; ++l)
+                outputMatrix[i][j] = 0;
+                for (int l = 0; l < matrix1Size.columns; ++l)
                 {
-                    _outputMatrix[i][j] += _matrix1[i][l] * _matrix2[l][j];
+                    outputMatrix[i][j] += matrix1[i][l] * matrix2[l][j];
                 }
             }
         }
         
-        var output = _outputMatrix.Select(n => string.Join(" ", n.Select(m => m.ToString()).ToArray()));
+        var output = outputMatrix.Select(n => string.Join(" ", n.Select(m => m.ToString()).ToArray()));
         
         File.WriteAllLines(outputPath, output);
     }
--- 1Homework07.09.22/ParallelMatrixMultiplication/ParallelMatrixMultiplication/NonMultipleMatricesException.cs
+++ 1Homework07.09.22/ParallelMatrixMultiplication/ParallelMatrixMultiplication/NonMultipleMatricesException.cs
@@ -1,5 +1,8 @@
 namespace ParallelMatrixMultiplication;
 
+/// <summary>
+/// These matrices cannot be multiplied.
+/// </summary>
 public class NonMultipleMatricesException : Exception
 {
     
