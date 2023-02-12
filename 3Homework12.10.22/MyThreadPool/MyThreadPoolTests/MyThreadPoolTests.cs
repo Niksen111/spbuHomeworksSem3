513bea3c4d861f4c5433cd12a56888124e8ea45a
Niksen111 <nikiksen2003@gmail.com>
2023-02-12 16:54:54 +0300

    Added tests

--- 3Homework12.10.22/MyThreadPool/MyThreadPoolTests/MyThreadPoolTests.cs
+++ 3Homework12.10.22/MyThreadPool/MyThreadPoolTests/MyThreadPoolTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 public class MyThreadPoolTests
@@ -108,7 +109,7 @@ public class MyThreadPoolTests
             return 1;
         });
 
-        var func2 = new Func<int, int>((x) =>
+        var func2 = new Func<int, int>(x =>
         {
             Thread.Sleep(500);
             return 2 * x;
@@ -157,5 +158,117 @@ public class MyThreadPoolTests
         Assert.Less(stopwatch.ElapsedMilliseconds, 10000);
 
         pool.Shutdown();
+    }
+
+    [Test]
+    public async Task ParallelSubmitsWorks()
+    {
+        MyThreadPool pool = new();
+        var func1 = new Func<int>(() =>
+        {
+            Thread.Sleep(50);
+            return 1;
+        });
+
+        var tasks = new Task<int>[100];
+        for (int i = 0; i < 100; ++i)
+        {
+            tasks[i] = Task.Run(() => pool.Submit(() => func1()).Result);
+        }
+
+        for (int i = 0; i < 100; ++i)
+        {
+            Assert.AreEqual(1, await tasks[i]);
+        }
+
+        pool.Shutdown();
+    }
+
+    [Test]
+    public async Task ParallelSubmitAdnShutDownWorks()
+    {
+        MyThreadPool pool = new();
+        var func1 = new Func<int>(() =>
+        {
+            Thread.Sleep(50);
+            return 1;
+        });
+
+        var tasks = new Task<int>[100];
+        Task? task1 = null;
+        for (int i = 0; i < 100; ++i)
+        {
+            if (i == 50)
+            {
+                task1 = Task.Run(() => pool.Shutdown());
+            }
+
+            tasks[i] = Task.Run(() => pool.Submit(() => func1()).Result);
+        }
+
+        for (int i = 0; i < 100; ++i)
+        {
+            int result;
+            var invalidOperation = new InvalidOperationException().GetType();
+            try
+            {
+                result = tasks[i].Result;
+                Assert.AreEqual(1, result);
+            }
+            catch (AggregateException e)
+            {
+                Assert.AreEqual(1, e.InnerExceptions.Count);
+                Assert.AreEqual(invalidOperation, e.InnerException!.GetType());
+            }
+        }
+
+        Assert.NotNull(task1);
+        if (task1 == null)
+        {
+            return;
+        }
+
+        await task1;
+        Assert.IsNull(task1.Exception);
+
+        pool.Shutdown();
+    }
+
+    [Test]
+    public Task ParallelSubmitsContinueWithAndShutDownWorks()
+    {
+        MyThreadPool pool = new(1);
+        var func1 = new Func<int>(() =>
+        {
+            Thread.Sleep(50);
+            return 1;
+        });
+        var func2 = new Func<int, int>(x =>
+        {
+            Thread.Sleep(50);
+            return 2 * x;
+        });
+
+        var taskSubmit = Task.Run(() => pool.Submit(() => func1()));
+        Assert.AreEqual(1, taskSubmit.Result.Result);
+        var taskShutDown = Task.Run(() => pool.Shutdown());
+        var taskContinueWith = Task.Run(() => taskSubmit.Result.ContinueWith(func2));
+
+        int result;
+        var invalidOperation = new InvalidOperationException().GetType();
+        try
+        {
+            result = taskContinueWith.Result.Result;
+            Assert.AreEqual(2, result);
+        }
+        catch (AggregateException e)
+        {
+            Assert.AreEqual(1, e.InnerExceptions.Count);
+            Assert.AreEqual(invalidOperation, e.InnerException!.GetType());
+        }
+
+        Assert.IsNull(taskShutDown.Exception);
+        pool.Shutdown();
+        return Task.CompletedTask;
     }
 }
